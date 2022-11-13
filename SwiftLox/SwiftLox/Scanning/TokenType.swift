403c73f5aaa2935b47782d16cdfaa07bdaf745d1
Ranveer <ranbanrambo@gmail.com>
2022-11-14 10:54:33 +1100

    🧼 Abstracted the concept of a token type to a protocol
    Abstraction arose after different tokens needed to be treated differently, but it was also desirable to have some shared behaviour.

--- SwiftLox/SwiftLox/Scanning/TokenType.swift
+++ SwiftLox/SwiftLox/Scanning/TokenType.swift
@@ -7,23 +7,76 @@
 
 import Foundation
 
-enum TokenType: String {
-    // MARK: Single Character
-    case leftParenthesis, rightParenthesis
-    case leftBracem, rightBrace
-    case comma, dot
-    case minus, plus, slash, star
-    case semicolon
+protocol TokenType {
+    var rawValue: String { get }
+}
+
+extension TokenType where Self: CaseIterable {
+    static var rawTokens: [String] { Self.allCases.map { $0.rawValue } }
+}
+
+
+// MARK: Single Character
+enum SingleCharToken: String, CaseIterable, TokenType {
+    case leftParenthesis = "("
+    case rightParenthesis = ")"
+    case leftBrace = "{"
+    case rightBrace = "}"
+    case comma = ","
+    case dot = "."
+    case minus = "-"
+    case plus = "+"
+    case star = "*"
+    case semicolon = ";"
+}
+  
+// MARK: Multi-character
+enum PotentiallyMuliCharOperatorToken: String, CaseIterable, TokenType {
+    case bang = "!"
+    case equal = "="
+    case greater = ">"
+    case less = "<"
     
-    // MARK: Multi-character
-    case bang, bandEqual
-    case equal, equalEqual
-    case greater, greaterEqual
-    case less, lessEqual
+    static let succeedingTokenRawForMultiCharOperator = "="
     
-    // MARK: Literals
+    var matchingMultiCharOperator: MultiCharOperatorToken {
+        switch self {
+        case .bang: return .bangEqual
+        case .equal: return .equalEqual
+        case .greater: return .greaterEqual
+        case .less: return .lessEqual
+        }
+    }
+}
+
+enum MultiCharOperatorToken: String, CaseIterable, TokenType {
+    case bangEqual = "!="
+    case equalEqual = "=="
+    case greaterEqual = ">="
+    case lessEqual = "<="
+    
+    /// Despite being unused, the presence of this variable ensures the two enums do not go out of sync (i.e. a case is added to an enum without the respective case being added to the matching enum)
+    var matchingSingleCharOperator: PotentiallyMuliCharOperatorToken {
+        switch self {
+        case .bangEqual: return .bang
+        case .equalEqual: return .equal
+        case .greaterEqual: return .greater
+        case .lessEqual: return .less
+        }
+    }
+}
+
+enum SpecialOperatorToken: String, CaseIterable, TokenType {
+    case slash = "/"
+}
+
+// MARK: Literals
+enum Literals: String, CaseIterable, TokenType {
     case identifier, string, number
-    
+}
+
+
+enum Keywords: String, CaseIterable, TokenType {
     // MARK: Keywords
     case and, or
     case `if`, `else`
