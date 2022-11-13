1b34068c499327fc569464ef5424d0d2227b739b
Ranveer <ranbanrambo@gmail.com>
2022-11-13 16:41:18 +1100

    🪄 Created object to represent the concept of a token

---
+++ SwiftLox/SwiftLox/Tokens/Token.swift
@@ -0,0 +1,21 @@
+//
+//  Token.swift
+//  SwiftLox
+//
+//  Created by Ranveer Mamidpelliwar on 23/9/2022.
+//
+
+import Foundation
+
+struct Token {
+    let tokenType: TokenType
+    let lexme: String
+    let literal: String
+    let line: Int
+}
+
+extension Token: CustomStringConvertible {
+    var description: String {
+        tokenType.rawValue + " " + lexme + " " + literal
+    }
+}
