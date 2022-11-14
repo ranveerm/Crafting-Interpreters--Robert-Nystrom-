2a05d4c007415a98ab661710d788fbacfadeed71
Ranveer <ranbanrambo@gmail.com>
2022-11-14 17:20:34 +1100

    🧪 Added basic test for scanning logic

---
+++ SwiftLox/SwiftLoxTests/ScannerTests.swift
@@ -0,0 +1,48 @@
+//
+//  ScannerTests.swift
+//  SwiftLoxTests
+//
+//  Created by Ranveer Mamidpelliwar on 14/11/2022.
+//
+
+import XCTest
+
+final class ScannerTests: XCTestCase {
+    var sut: Scanner!
+    
+    override func setUpWithError() throws {
+        self.sut = Scanner("")
+    }
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func test_scanForTokenBasic() throws {
+        /// Given
+        let inputText = """
++ - {
+*
+< == (
+// This is a comment
+/ \t \n
+"""
+        let sut = Scanner(inputText)
+        let expectedTokens: [Token] = [
+            Token(tokenType: SingleCharToken.plus, lexme: "+", literal: nil, line: 1),
+            Token(tokenType: SingleCharToken.minus, lexme: "-", literal: nil, line: 1),
+            Token(tokenType: SingleCharToken.leftBrace, lexme: "{", literal: nil, line: 1),
+            Token(tokenType: SingleCharToken.star, lexme: "*", literal: nil, line: 2),
+            Token(tokenType: PotentiallyMuliCharOperatorToken.less, lexme: "<", literal: nil, line: 3),
+            Token(tokenType: MultiCharOperatorToken.equalEqual, lexme: "==", literal: nil, line: 3),
+            Token(tokenType: SingleCharToken.leftParenthesis, lexme: "(", literal: nil, line: 3),
+            Token(tokenType: SpecialOperatorToken.slash, lexme: "/", literal: nil, line: 5),
+            Token(tokenType: SpecialToken.eof, lexme: "", literal: nil, line: 6)
+        ]
+        
+        /// When
+        let computedTokens = try sut.scanForTokens()
+        
+        /// Then
+        zip(expectedTokens, computedTokens).forEach { XCTAssertEqual($0, $1) }
+    }
+}
