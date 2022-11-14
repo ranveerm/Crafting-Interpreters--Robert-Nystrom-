8040c675d3ecde5cf870ba830d14d68b574a2a16
Ranveer <ranbanrambo@gmail.com>
2022-11-14 17:17:57 +1100

    🍴 Added conformance to equatable

--- SwiftLox/SwiftLox/Scanning/Token.swift
+++ SwiftLox/SwiftLox/Scanning/Token.swift
@@ -14,6 +14,12 @@ struct Token {
     let line: Int
 }
 
+extension Token: Equatable {
+    static func == (lhs: Token, rhs: Token) -> Bool {
+        lhs.tokenType.rawValue == rhs.tokenType.rawValue && lhs.lexme == rhs.lexme && lhs.literal == rhs.literal && lhs.line == rhs.line
+    }
+}
+
 extension Token: CustomStringConvertible {
     var description: String {
         "[\(line)] " + tokenType.description + "\t\t" + lexme + " " + (literal ?? "")
