7c40fa20581ac06e63069df9a86fec149cdad839
Ranveer <ranbanrambo@gmail.com>
2022-11-21 14:41:06 +1100

    🪄 Specified limited set of concrete types for an expression abstraction

--- SwiftLox/SwiftLox/Parsing/Expr.swift
+++ SwiftLox/SwiftLox/Parsing/Expr.swift
@@ -8,3 +8,24 @@
 import Foundation
 
 protocol Expr { }
+
+// MARK: Concrete Types
+struct BinaryExpr: Expr {
+    let lhs: Expr
+    let rhs: Expr
+    let `operator`: TokenType
+}
+
+struct UnaryExpr: Expr {
+    let `operator`: TokenType
+    let rhs: Expr
+}
+
+struct GroupingExpr: Expr {
+    let expr: Expr
+}
+
+struct LiteralExpr: Expr {
+    let type: Literals
+    let value: String
+}
