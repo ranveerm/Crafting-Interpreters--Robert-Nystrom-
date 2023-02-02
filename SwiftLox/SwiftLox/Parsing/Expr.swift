67857bfcb7f3862be681109d38cd816a467359e6
Ranveer <ranbanrambo@gmail.com>
2023-02-02 19:02:32 +1100

    🔗 Incorporated visitor interface

--- SwiftLox/SwiftLox/Parsing/Expr.swift
+++ SwiftLox/SwiftLox/Parsing/Expr.swift
@@ -7,7 +7,14 @@
 
 import Foundation
 
-protocol Expr { }
+/**
+ Container to server as a mechanism to group concrete expression types.
+ - Note: There is no specification for state or method specific to the concept of expression as this protocol purely serves as a grouping mechanism. However, it does act as an Visitor interface for expression types (see [Visitor Pattern](https://refactoring.guru/design-patterns/visitor) for more details).
+ */
+protocol Expr {
+    func acceptVisitor<T: ExprVisitor>(_ visitor: T) -> T.VisitorReturnType
+}
+
 // MARK: Grammar
 /*:
  # Grammar
@@ -33,18 +40,26 @@ struct BinaryExpr: Expr {
     let lhs: Expr
     let rhs: Expr
     let `operator`: TokenType
+    
+    func acceptVisitor<T>(_ visitor: T) -> T.VisitorReturnType where T : ExprVisitor { visitor.visitBinaryExpr(expr: self) }
 }
 
 struct UnaryExpr: Expr {
     let `operator`: TokenType
     let rhs: Expr
+    
+    func acceptVisitor<T>(_ visitor: T) -> T.VisitorReturnType where T : ExprVisitor { visitor.visitUnaryExpr(expr: self) }
 }
 
 struct GroupingExpr: Expr {
     let expr: Expr
+    
+    func acceptVisitor<T>(_ visitor: T) -> T.VisitorReturnType where T : ExprVisitor { visitor.visitGroupingExpr(expr: self) }
 }
 
 struct LiteralExpr: Expr {
     let type: Literals
     let value: String
+    
+    func acceptVisitor<T>(_ visitor: T) -> T.VisitorReturnType where T : ExprVisitor { visitor.visitLiteralExpr(expr: self) }
 }
