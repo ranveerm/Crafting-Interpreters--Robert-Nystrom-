b2247b512e521906630292eac386af721d84db1f
Ranveer <ranbanrambo@gmail.com>
2023-02-27 22:57:22 +1100

    🪄 Created abstraction for token

--- SwiftLox/SwiftLox/Scanning/Token.swift
+++ SwiftLox/SwiftLox/Scanning/Token.swift
@@ -8,15 +8,33 @@
 import Foundation
 
 struct Token {
-    let tokenType: TokenType
-    let lexme: String
-    let literal: String?
+protocol AbstractToken: CustomStringConvertible {
+    associatedtype Group: LexmeGroup
+    var lexmeGroup: Group { get }
+    var lexme: Lexme { get }
+    var literal: String? { get }
+    var line: Int { get }
+}
+
+// MARK: Default Implementation
+extension AbstractToken {
+    var lexme: Lexme { lexmeGroup.lexme }
+}
+
+/// CustomStringConvertible
+extension AbstractToken {
+    var description: String {
+        "[\(line)]".padding(toLength: 6, withPad: " ", startingAt: 0) + lexme.description.padding(toLength: 30, withPad: " ", startingAt: 0) + (literal ?? lexme.rawValue)
+    }
+}
+
     let line: Int
 }
 
+// TODO: Determine if obviating `lexmeGroup` from the equality check is valid
 extension Token: Equatable {
     static func == (lhs: Token, rhs: Token) -> Bool {
-        lhs.tokenType.rawValue == rhs.tokenType.rawValue && lhs.lexme == rhs.lexme && lhs.literal == rhs.literal && lhs.line == rhs.line
+        lhs.lexme == rhs.lexme && lhs.literal == rhs.literal && lhs.line == rhs.line
     }
 }
 
